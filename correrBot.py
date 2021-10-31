92199ab43a387ddd3e37d62523a79db53afd4429
JoselBOSS <84639023+joselboss@users.noreply.github.com>
2021-10-31 11:58:53 -0500

    Update correrBot.py

--- correrBot.py
+++ correrBot.py
@@ -1,1 +1,11 @@
+#Hay una forma alternativa más segura, pronto lo haré o puede que lo haga ya xD
 
+import discord
+
+token = 'TOKEN DE TU BOT'
+
+def correrBot():
+  print('El bot esta corriendo...')
+  client.run(token)
+  
+correrBot()
